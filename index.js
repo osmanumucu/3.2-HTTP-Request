793e6fc3467ad077ef6d34c913f0e7a53b8d3866
Osman Talha Umucu <osmanumucu40@gmail.com>
2024-05-29 10:58:24 +0200

    feat: running different endpoints successfull

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ import express from "express"
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("Well Done!")
+})
+
+app.get("/about", (req, res) => {
+    res.send("About Me!")
+})
+
+app.get("/contact", (req, res) => {
+    res.send("Contact!")
+})
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
